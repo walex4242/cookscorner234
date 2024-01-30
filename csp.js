6982fda06eef8cd37706845ae17bd31bf57397e2
Olawale Olafisoye <walex.world20@gmail.com>
2024-01-30 07:04:18 -0300

    Update csp.js

--- csp.js
+++ csp.js
@@ -7,6 +7,8 @@ const policies = {
     'https://checkout.stripe.com',
     'https://js.stripe.com',
     'https://maps.googleapis.com',
+    'https://cookscorner.payloadcms.app',
+    'https://cookscornernc.com',
   ],
   'child-src': ["'self'"],
   'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
@@ -23,12 +25,16 @@ const policies = {
     'https://checkout.stripe.com',
     'https://js.stripe.com',
     'https://hooks.stripe.com',
+    'https://cookscornernc.com',
+    'https://cookscorner.payloadcms.app',
   ],
   'connect-src': [
     "'self'",
     'https://checkout.stripe.com',
     'https://api.stripe.com',
     'https://maps.googleapis.com',
+    'https://cookscorner.payloadcms.app',
+    'https://cookscornernc.com',
   ],
 }
 
