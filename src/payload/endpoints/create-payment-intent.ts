cc0b01e2179334728be4b4a9ee93ae27c8a9a196
Olawale Olafisoye <walex.world20@gmail.com>
2024-02-07 14:38:13 -0300

    Update create-payment-intent.ts

--- src/payload/endpoints/create-payment-intent.ts
+++ src/payload/endpoints/create-payment-intent.ts
@@ -1,14 +1,16 @@
-/* eslint-disable function-paren-newline */
-/* eslint-disable prettier/prettier */
 import type { PayloadHandler } from 'payload/config'
 import Stripe from 'stripe'
 
-import type { CartItems, Product } from '../payload-types'
-const categoriesWithSelectedProducts: Set<string> = new Set()
+import type { CartItems } from '../payload-types'
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: '2022-08-01',
 })
 
+// this endpoint creates a `PaymentIntent` with the items in the cart
+// to do this, we loop through the items in the cart and lookup the product in Stripe
+// we then add the price of the product to the total
+// once completed, we pass the `client_secret` of the `PaymentIntent` back to the client which can process the payment
 export const createPaymentIntent: PayloadHandler = async (req, res): Promise<void> => {
   const { user, payload } = req
 
@@ -56,6 +58,7 @@ export const createPaymentIntent: PayloadHandler = async (req, res): Promise<voi
       throw new Error('No items in cart')
     }
 
+    // for each item in cart, lookup the product in Stripe and add its price to the total
     await Promise.all(
       fullUser?.cart?.items?.map(async (item: CartItems[0]): Promise<null> => {
         const { product, quantity } = item
@@ -64,76 +67,28 @@ export const createPaymentIntent: PayloadHandler = async (req, res): Promise<voi
           return null
         }
 
-        if (typeof product === 'string') {
-          const productDoc = await payload.findByID({
-            collection: 'products',
-            id: product,
-          })
-
-          if (productDoc?.categories && productDoc.categories.length > 0) {
-            productDoc.categories.forEach(category => {
-              if (typeof category === 'string') {
-                categoriesWithSelectedProducts.add(category)
-              } else {
-                categoriesWithSelectedProducts.add(category.id)
-              }
-            })
-          }
-        } else if (product?.stripeProductID) {
-          // Handle the case where product is a Product
-          const prices = await stripe.prices.list({
-            product: product.stripeProductID,
-            limit: 100,
-            expand: ['data.product'],
-          })
-
-          if (prices.data.length === 0) {
-            res.status(404).json({ error: 'There are no items in your cart to checkout with' })
-            return null
-          }
-
-          const price = prices.data[0]
-          total += price.unit_amount * quantity
-
-          // Track the categories with selected products
-          if (product.categories && product.categories.length > 0) {
-            product.categories.forEach(category => {
-              if (typeof category === 'string') {
-                categoriesWithSelectedProducts.add(category)
-              } else {
-                categoriesWithSelectedProducts.add(category.id)
-              }
-            })
-          }
+        if (typeof product === 'string' || !product?.stripeProductID) {
+          throw new Error('No Stripe Product ID')
         }
+
+        const prices = await stripe.prices.list({
+          product: product.stripeProductID,
+          limit: 100,
+          expand: ['data.product'],
+        })
+
+        if (prices.data.length === 0) {
+          res.status(404).json({ error: 'There are no items in your cart to checkout with' })
+          return null
+        }
+
+        const price = prices.data[0]
+        total += price.unit_amount * quantity
 
         return null
       }),
     )
-    const allCategories = fullUser?.cart?.items
-      .flatMap(item => {
-        const product = item.product as Product
-        return typeof product === 'string' ? [] : ((product?.categories || []) as string[])
-      })
-      .filter((category, index, array) => array.indexOf(category) === index)
 
-    const categoriesSelectedInCart = (fullUser?.cart?.items || [])
-      .filter(item => typeof item.product !== 'string' && item.product?.categories)
-      .flatMap(item => {
-        const product = item.product as Product
-        return product?.categories || []
-      })
-      .filter((category, index, array) => array.indexOf(category) === index)
-
-    const isAllCategoriesSelected = allCategories.every(category =>
-      categoriesSelectedInCart.includes(category),
-    )
-
-    if (!isAllCategoriesSelected) {
-      throw new Error(
-        'Please pick at least one product from each category before making a payment.',
-      )
-    }
     if (total === 0) {
       throw new Error('There is nothing to pay for, add some items to your cart and try again.')
     }
@@ -149,6 +104,6 @@ export const createPaymentIntent: PayloadHandler = async (req, res): Promise<voi
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : 'Unknown error'
     payload.logger.error(message)
-    res.status(400).json({ error: message })
+    res.json({ error: message })
   }
 }
